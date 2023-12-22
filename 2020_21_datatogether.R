5640eeee584e68d5224fa7ee4d8cf51a7b49e87a
aecahill <aecsk8gr8@yahoo.com>
2023-12-22 12:56:08 -0500

    changes_sicb2024
    December 22 changes to data and code

--- 2020_21_datatogether.R
+++ 2020_21_datatogether.R
@@ -2,8 +2,8 @@
 #need file marshsites 
 #THIS CODE NOW REQUIRES THE RAREFACTION SCRIPT ALSO!
 
-river<-read.table("C:/Users/aecsk/Desktop/river_taxa.txt",header=TRUE)
-riversites<-read.table("C:/Users/aecsk/Desktop/river_sites.txt",header=TRUE)
+river<-read.table("C:/Users/aecsk/Documents/GitHub/Wildrice_code/river_taxa.txt",header=TRUE)
+riversites<-read.table("C:/Users/aecsk/Documents/GitHub/Wildrice_code/river_sites.txt",header=TRUE)
 
 #load vegan
 library(vegan)
@@ -24,15 +24,16 @@ rivernmds<-metaMDS(river)
 #analysis of similarity for sites and regions
 anosim(river,riversites$Month)
 anosim(river,riversites$Enviro)
+anosim(river,riversites$Rice)
 
 #compute PERMANOVA with a space and time interaction
-adonis(formula=river~riversites$Month*riversites$Enviro)
+adonis(formula=river~riversites$Month*riversites$Rice)
 
 
 
 #moving plot to ggplot
 
-data.scores <- as.data.frame(scores(rivernmds))
+data.scores <- as.data.frame(scores(rivernmds)$sites)
 datascores<-cbind(data.scores,riversites)
 head(datascores)
 species.scores <- as.data.frame(scores(rivernmds, "species"))
@@ -97,7 +98,7 @@ grp.j <- data.scores[datascores$Enviro == "j_wildricebed_bridge", ][chull(datasc
                                                                                        "j_wildricebed_bridge", c("NMDS1", "NMDS2")]), ]
 
 hull.data <- rbind(grp.a, grp.b, grp.c, grp.d,grp.e, grp.f, grp.g, grp.h,grp.i,grp.j) #turn the hulls into a single dataframe
-hull.sample<-c("A","A","A","A","A","B","B","B","B","C","C","C","C","D","D","D","D","D","E","E","E","E","E","E","F","F","F","F","G","G","G","G","G","H","H","H","H","I","I","I","I","I","I","J","J","J","J") #add column for groups (these are based on this data only)
+hull.sample<-c("A","A","A","A","A","B","B","B","B","B","C","C","C","C","D","D","D","D","D","E","E","E","E","E","E","F","F","F","F","G","G","G","G","G","H","H","H","H","I","I","I","I","I","I","J","J","J","J") #add column for groups (these are based on this data only)
 hull.data<-cbind(hull.data,hull.sample) #attach group names to hull dataframe
 
 #plot in ggplot
@@ -119,14 +120,39 @@ morphosite<-ggplot() +
   geom_polygon(data=hull.data,aes(x=NMDS1,y=NMDS2,group=hull.sample),alpha=0.10) #add polygon based on the hulls calculated
 
 
+#By rice
+grp.a <- data.scores[datascores$Rice == "Y", ][chull(datascores[datascores$Rice == 
+                                                                      "Y", c("NMDS1", "NMDS2")]), ]
+grp.b <- data.scores[datascores$Rice == "N", ][chull(datascores[datascores$Rice == 
+                                                                        "N", c("NMDS1", "NMDS2")]), ]
+hull.data <- rbind(grp.a, grp.b) #turn the hulls into a single dataframe
+hull.rice<-c("Y","Y","Y","Y","Y","Y","Y","Y","Y","N","N","N","N","N","N") #add column for groups (these are based on this data only)
+hull.data<-cbind(hull.data,hull.rice) #attach group names to hull dataframe
 
-plot_grid(morphosite,morphotime,labels=c("A","B"),ncol=1)
+morphorice<-ggplot() +
+  geom_point(data=datascores,aes(x=NMDS1,y=NMDS2,colour=Rice),size=3) + # add the point markers
+  scale_colour_manual(values=rev(wes_palette("Zissou1", n = 2, type="continuous"))) +
+  coord_equal() +
+  theme_bw()+
+  theme(axis.text.x = element_blank(),  # remove x-axis text
+        axis.text.y = element_blank(), # remove y-axis text
+        axis.ticks = element_blank(),  # remove axis ticks
+        axis.title.x = element_text(size=16), # remove x-axis labels
+        axis.title.y = element_text(size=16), # remove y-axis labels
+        panel.background = element_blank(), 
+        panel.grid.major = element_blank(),  #remove major-grid labels
+        panel.grid.minor = element_blank(),  #remove minor-grid labels
+        plot.background = element_blank())+ 
+  geom_polygon(data=hull.data,aes(x=NMDS1,y=NMDS2,group=hull.rice),alpha=0.10) #add polygon based on the hulls calculated
+
+plot_grid(morphosite,morphotime,morphorice,labels=c("A","B","C"),ncol=1)
+
 
 #diversity statistics
 
 riverdiv<-cbind(diversity(river,index="simpson"),riversites) #calculate simpsons index, bind to site information
 
-colnames(riverdiv)<-c("Simpsons","Enviro","Month","Site","Replicate") #rename columns
+colnames(riverdiv)<-c("Simpsons","Enviro","Month","Site","Replicate","Rice") #rename columns
 
 
 #summary(aov(marshdiv$Simpsons~marshdiv$Month)) #anova among regions
